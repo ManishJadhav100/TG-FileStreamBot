a86719f8ae83b64f3a95d5161232e6b4c0e1acf3
Manish Jadhav <54273848+ManishJadhav100@users.noreply.github.com>
2021-12-20 16:13:59 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,12 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Var(object):
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = 3796974
+    API_HASH = '9511d0112631f9990337eb724d1a7d0d'
+    BOT_TOKEN = '5005485389:AAEHIWantBXQ5LOg9yHgz68KnkbiCu4mrYk'
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '3'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', None))     
+    BIN_CHANNEL = -1001771465683     
     PORT = int(getenv('PORT', 8080))
     BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '1200')) # 20 minutes
@@ -22,7 +22,7 @@ class Var(object):
     NO_PORT = True if str(NO_PORT).lower() == 'true' else False
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
+        APP_NAME = 'mjfiletolinkbot'
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADDRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME + '.herokuapp.com'
